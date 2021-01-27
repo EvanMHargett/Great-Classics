d8f058ad579cd80c560c425a69a071411eb8af2a
Evanmhargett <evanmhargett@gmail.com>
2021-01-27 16:03:58 -0500

    Worked around the issue, no longer using update. Deleting old entries instead

--- backend/routes/api/bookshelf.js
+++ backend/routes/api/bookshelf.js
@@ -6,22 +6,24 @@ const { BookshelfBooks } = require('../../db/models');
 
 router.post('/', asyncHandler(async (req, res, next) =>{
     const {bookId, currentBookshelfId, nextBookshelfId} = req.body
-    console.log("BookId", bookId, typeof bookId)
-    console.log("currentBookshelfId", currentBookshelfId, typeof currentBookshelfId)
-    console.log("nextBookshelfId", nextBookshelfId, typeof nextBookshelfId)
     if(nextBookshelfId === -1){
         const bookshelfBook = await BookshelfBooks.findOne({where: {bookId, bookshelfId: currentBookshelfId}})
         await bookshelfBook.destroy()
     } 
-    else if(currentBookshelfId === -1){
-        const bookshelfBook = await BookshelfBooks.create({bookId, bookshelfId: nextBookshelfId})
+    else {
+        if(currentBookshelfId !== -1)
+        {
+            const bookshelfBook = await BookshelfBooks.findOne({where: {bookId, bookshelfId: currentBookshelfId}})
+            await bookshelfBook.destroy()
+        }
+        await BookshelfBooks.create({bookId, bookshelfId: nextBookshelfId})
     }
-    else{
-        console.log("updating entry")
-        const bookshelfBookToUpdate = await BookshelfBooks.findOne({where: {bookId, bookshelfId: currentBookshelfId}})
-        bookshelfBookToUpdate.bookshelfId = nextBookshelfId
-        await bookshelfBookToUpdate.save()
-    }
+    // else{
+    //     console.log("updating entry")
+    //     const bookshelfBookToUpdate = await BookshelfBooks.findOne({where: {bookId, bookshelfId: currentBookshelfId}})
+    //     await bookshelfBookToUpdate.update({bookshelfId: 1})
+    //     console.log("asd", bookshelfBookToUpdate)
+    // }
 
 }))
 
