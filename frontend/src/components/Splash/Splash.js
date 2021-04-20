15fa7e5a9673a7f8c0fef388696140fb7a4a50cb
Evanmhargett <evanmhargett@gmail.com>
2021-04-20 12:51:00 -0400

    Like the about bar a lot more now

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -10,7 +10,7 @@ export default function Splash(){
                 <p>
                     <Link to='/books' className="pure-button splash-button">See all books</Link>
                 </p>
-                <div className="about">Created by Evan Hargett <a href="https://github.com/EvanMHargett"><img className="gh" src="/images/GitHub.png"></img></a><a href="https://www.linkedin.com/in/evan-hargett-47723b162/"><img className="li" src="/images/Linkedin.png"></img></a></div>
+                <div className="about"> <span className="name"><p>Created By:</p><p>Evan Hargett</p></span> <a href="https://github.com/EvanMHargett"><img className="gh" src="/images/GitHub.png"></img></a><a href="https://www.linkedin.com/in/evan-hargett-47723b162/"><img className="li" src="/images/Linkedin.png"></img></a></div>
             </div>
             
         </div>
